d140d16b9445dcad572252cfcf27d80f80095963
minjichu <43025965+minjichu@users.noreply.github.com>
2022-10-18 23:22:54 +0900

    Create address.h
    version 1

---
+++ DataStructure/MakingAddressBook/address.h
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#ifndef __ADDRESS_H__
+#define __ADDRESS_H__
+#define MAX_SIZE 20
+
+unsigned long bookNum;
+unsigned long last_index;
+typedef struct book {
+	unsigned long index;
+	char name[MAX_SIZE];
+	char phoneNum[MAX_SIZE];
+	char address[MAX_SIZE];
+	struct book * next;
+}book;
+
+void init_book();
+void clear_book();
+int delete_book(int l);
+int add_book(book data);
+void printAll_book();
+void view_book(book * data);
+
+
+
+
+#endif
